077b25166867d60493a818848c148f8283c86f69
PjeterPSK1 <ped1567ro@gmail.com>
2023-06-17 12:38:14 -0300

    Feat: mudanças estéticas e aplicação do conceito de css modules para a home

--- src/components/finder.tsx
+++ src/components/finder.tsx
@@ -1,5 +1,7 @@
 import nb from '../assets/finder-nb-cel.png'
 import classes from './finder.module.css'
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 
@@ -7,9 +9,11 @@ const finder = () => {
 
     return (
         <div className={classes.finder}>
-            <h1>Finder</h1>
+            <a href={"https://github.com/PedroPSKOliveira/finder"} target={"_blank"} style={{ textDecoration: 'none', color: '#000000' }}>
+                <h1>Finder <FontAwesomeIcon icon={faGithub} style={{color: "#000000"}} /></h1>
+            </a>
             <a href={"https://finder-delta.vercel.app/"} target={"_blank"}>
-            <img src={nb} alt="Finder" className={classes.imagem}/>
+                <img src={nb} alt="Finder" className={classes.imagem}/>
             </a>
             <p>Finder é um projeto que eu fiz para treinar minhas habilidades com React, TypeScript, CSS Modules e Vite. Ele é um site que busca usuários do GitHub e mostra algumas informações sobre eles.</p>
         </div>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,37 +2,42 @@ import eu from '../assets/eu.jpg'
 import Finder from "../components/finder.tsx";
 import { faGithub, faLinkedin, faWhatsapp} from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faReact} from "@fortawesome/free-brands-svg-icons/faReact";
+import classes from './Home.module.css'
 
 const Home = () => {
     return (
         <>
         <body>
-        <div className="page-wrap">
-            <div className="container">
-                <div className="main">
-                    <div className="main-left">
-                        <img src={eu} alt="Foto de perfil" className="profile-picture"/>
+        <div className={classes.pageWrap}>
+            <div className={classes.container}>
+                <div className={classes.main}>
+                    <div>
+                        <img src={eu} alt="Foto de perfil" className={classes.profilePicture}/>
 
-                            <div className="name-social">
-                                <h1 className="name">Pedro Almeida</h1>
-                                <div className="social-icons">
+                            <div className={classes.nameSocial}>
+                                <h1 className={classes.name}>Pedro Almeida✌️</h1>
+                                <div className={classes.socialIcons}>
                                     <a href="https://github.com/PedroPSKOliveira" target={"_blank"}><FontAwesomeIcon icon={faGithub} size="xl" style={{color: "#000000"}} /></a>
                                     <a href="https://www.linkedin.com/in/pedro-almeida-oliveira-87b0a3246/" target={"_blank"}><FontAwesomeIcon icon={faLinkedin} size="xl" style={{color: "#000000",}} /></a>
                                     <a href="https://api.whatsapp.com/send/?phone=5573981427266&text&type=phone_number&app_absent=0" target={"_blank"}><FontAwesomeIcon icon={faWhatsapp} size="xl" style={{color: "#000000",}} /></a>
                                 </div>
                             </div>
 
-                            <h2 className="job">Desenvolvedor React</h2>
-                            <p className="description">TENHO 19 ANOS SOU DESENVOLVEDOR FRONTEND COM 1 ANO DE EXPERIÊNCIA
+                        <h2 className={classes.job}>
+                            <span>Desenvolvedor React</span>
+                            <FontAwesomeIcon icon={faReact} spin size="xl" style={{color: "#00d5ff"}} />
+                        </h2>
+                            <p className={classes.description}>TENHO 19 ANOS SOU DESENVOLVEDOR FRONTEND COM 1 ANO DE EXPERIÊNCIA
                                 TRABALHANDO EM ESTÁGIOS E EM START-UP</p>
                     </div>
                 </div>
-                <div className="chamada">
+                <div className={classes.chamada}>
                     <h1>Veja mais sobre os meus projetos 👇</h1>
                 </div>
             </div>
                 <Finder/>
-            <div className={"projeto2"}>
+            <div className={classes.projeto2}>
                 <h1>Em breve</h1>
             </div>
         </div>
